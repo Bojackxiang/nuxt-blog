f681d305b2ba306740dd2dca211019eb75f1a28f
Alex <alex@q-83.com>
2021-06-05 00:35:52 +1000

    为 header 添加了 图片

---
+++ components/PostPreview.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="post-preview-wrapper">
+    <div class="post-preview-wrapper__bg"></div>
+    <div class="post-preview-wrapper__header">
+      <h4 class="post-preview-wrapper__header__title">
+        {{ title }}
+      </h4>
+      <div>{{ this.createdAt }}</div>
+    </div>
+
+    <div>
+      {{ content }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "post-preview",
+  props: {
+    postId: { type: String, required: true },
+  },
+  data() {
+    return {
+      title: "",
+      content: "",
+      createdAt: "",
+    };
+  },
+  mounted() {
+    this.title = "Test previuew";
+    this.content = "Test comtent ...";
+    this.createdAt = new Date().toString();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.post-preview-wrapper {
+  font-size: 1.6rem;
+  &__bg{
+    background-image: url('~assets/images/header.jpeg');
+    min-height: 120px;;
+  }
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    background-color: aqua;
+    align-items: center;
+    &__title {
+      color: red;
+    }
+  }
+}
+</style>
