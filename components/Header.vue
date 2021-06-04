7e00d6dce6d8fb8586b79218c2871a6a470e522c
Alex <alex@q-83.com>
2021-06-05 00:49:10 +1000

    添加了自定义的 nuxt-link-active 的颜色

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header-wrapper">  
+    <nuxt-link to="/posts" class="header-wrapper__link-item">posts</nuxt-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/config.scss';
+a.nuxt-link-active{
+    color: red ;
+  }
+.header-wrapper{
+  background-color: $color_1;;
+  min-height: 50px;
+  display: flex;
+  align-items: center;
+  &__link-item{
+    margin: 10px;
+    color: $color_4; 
+    text-decoration: none;
+  }
+  
+}
+
+</style>
