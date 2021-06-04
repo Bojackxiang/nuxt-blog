29fc3036973bb5b8fe256998c1cc50048549883d
Alex <alex@q-83.com>
2021-06-04 11:43:40 +1000

    引入了sass来写css

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -34,6 +34,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/style-resources',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    
+  <div class="post-page">
+    <div class="post-page__header"></div>
   </div>
 </template>
 
@@ -11,5 +11,11 @@
 </script>
 
 <style lang="scss" scoped>
-
+.post-page{
+  min-height: 20px;
+  &__header{
+    min-height: 20px;
+    background-color: antiquewhite;
+  }
+}
 </style>
