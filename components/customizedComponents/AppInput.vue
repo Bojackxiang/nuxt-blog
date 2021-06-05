21bd0a73f7bfe33c1827dbb1547cf29e87380ce0
Alex <alex@q-83.com>
2021-06-05 11:53:04 +1000

    添加了自定义的 appinput

---
+++ components/customizedComponents/AppInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="input-wrapper">
+    <input 
+      type="text"
+      v-bind="$attrs"
+      @input="e => onInputChange(e)"
+    >
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      name: String,
+      value: String,
+      updateInput: Function,
+    },
+    data(){
+      return {
+        inputValue: ''
+      }
+    },
+    mounted(){
+      this.inputValue = this.value;
+    },
+    methods: {
+      onInputChange(e){
+        this.$emit('updateInput', this.name, e.target.value)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .input-wrapper{
+
+  }
+</style>
