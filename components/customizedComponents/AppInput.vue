5167df9bb7ea199b883efb14cbd4c54fa9052497
Alex <alex@q-83.com>
2021-06-05 12:05:33 +1000

    更新了自定义的input 组件

--- components/customizedComponents/AppInput.vue
+++ components/customizedComponents/AppInput.vue
@@ -1,38 +1,43 @@
 <template>
   <div class="input-wrapper">
-    <input 
+    <input
       type="text"
       v-bind="$attrs"
-      @input="e => onInputChange(e)"
-    >
+      :value="value"
+      @input="(e) => onInputChange(e)"
+    />
   </div>
 </template>
 
 <script>
-  export default {
-    props: {
-      name: String,
-      value: String,
-      updateInput: Function,
+export default {
+  props: {
+    value: String,
+  },
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+  mounted() {
+    this.inputValue = this.value;
+  },
+  methods: {
+    onInputChange(e) {
+      // REVIEW 下面的 这个 input 是不能变的
+      this.$emit("input", e.target.value);
     },
-    data(){
-      return {
-        inputValue: ''
-      }
-    },
-    mounted(){
-      this.inputValue = this.value;
-    },
-    methods: {
-      onInputChange(e){
-        this.$emit('updateInput', this.name, e.target.value)
-      }
-    }
-  }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .input-wrapper{
-
-  }
+.input-wrapper {
+}
 </style>
+
+
+// REVIEW <Vue 知识> 这边困惑了一会，相当于外面 v-model 一个 内部的数据
+// 进来一个 value 就完事了 （v-model）传捡来的
+// 内部组件在给一个 @input="(e) => onInputChange(e)"
+// 就能够和外面的 value 直接进行绑定了
---
+++ pages/auth/login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <AppInput 
+      name="'username'" 
+      v-model="username"
+    />
+    <p>{{username}}</p>
+  </div>
+</template>
+
+<script>
+import AppInput from '../../components/customizedComponents/AppInput.vue'
+  export default {
+    components:{
+      AppInput
+    },
+    data(){
+      return {
+        username: 'hello'
+      }
+    },
+    methods: {
+      updateInput(inputValue ){
+        console.log(inputValue);
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
