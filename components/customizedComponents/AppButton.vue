3eabd8cad95820d70aaf0ffedaf470551c90047f
Alex <alex@q-83.com>
2021-06-05 12:55:32 +1000

    使用 v-on="$listeners" 来直接坚挺进来的事件

---
+++ components/customizedComponents/AppButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="button" v-bind="$attrs" v-on="$listeners">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    click: {
+      type: Function,
+      required: false,
+    },
+  },
+  methods: {
+    onClickHandler() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
