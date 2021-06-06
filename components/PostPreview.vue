556392ccc0cd4b7950679b758ec2a29b051a7094
Alex <alex@q-83.com>
2021-06-06 20:49:49 +1000

    添加了 async Data 俩代替原有的data

--- components/PostPreview.vue
+++ components/PostPreview.vue
@@ -1,24 +1,34 @@
 <template>
-  <div class="post-preview-wrapper">
-    <div class="post-preview-wrapper__bg"></div>
-    <div class="post-preview-wrapper__header">
-      <h4 class="post-preview-wrapper__header__title">
-        {{ title }}
-      </h4>
-      <div>{{ this.createdAt }}</div>
-    </div>
-
-    <div>
-      {{ content }}
-    </div>
-  </div>
+  <el-row class="post-preview-wrapper">
+    <el-col
+      class="post-preview-wrapper__card-body"
+      :md="6"
+      :sm="12"
+      :xs="24"
+      v-for="number in 6"
+      :key="number"
+    >
+      <div class="post-preview-wrapper__bg" />
+      <div class="post-preview-wrapper__header">
+        <span class="post-preview-wrapper__header__title">
+          {{ title }}
+        </span>
+        <span class="post-preview-wrapper__header__createAt">{{
+          formatedDate
+        }}</span>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+import moment from "moment";
 export default {
   name: "post-preview",
   props: {
-    postId: { type: String, required: true },
+    title: { type: String, required: true },
+    content: { type: String, required: false },
+    createAt: { type: Date, required: true },
   },
   data() {
     return {
@@ -27,28 +37,38 @@ export default {
       createdAt: "",
     };
   },
-  mounted() {
-    this.title = "Test previuew";
-    this.content = "Test comtent ...";
-    this.createdAt = new Date().toString();
+  computed: {
+    formatedDate: () => moment(new Date()).format("YYYY/MM/DD"),
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+}
 .post-preview-wrapper {
   font-size: 1.6rem;
-  &__bg{
-    background-image: url('~assets/images/header.jpeg');
-    min-height: 120px;;
+  text-align: center;
+  &__card-body {
+  }
+  &__bg {
+    background-image: url("~assets/images/header.jpeg");
+    min-height: 120px;
   }
   &__header {
+    padding: 10px;
     display: flex;
     justify-content: space-between;
-    background-color: aqua;
-    align-items: center;
+    align-items: baseline;
     &__title {
-      color: red;
+      display: block;
+      font-size: 1.5rem;
+    }
+    &__createAt {
+      display: block;
+      font-size: 0.8rem;
     }
   }
 }
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,21 +1,48 @@
 <template>
   <div class="posts-page">
     <div class="post-page__header"></div>
+
+    <PostPreview
+      title="post.title"
+      content="post.content"
+      createAt="post.createAt"
+      v-for="(post, index) in posts"
+      :key="index"
+    />
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+  // REVIEW 在 page 里面使用 asyncdata 而不是 data，
+  // 因为data 会被 asyncData直接覆盖掉
+  asyncData({
+    isDev,
+    route,
+    store,
+    env,
+    params,
+    query,
+    req,
+    res,
+    redirect,
+    error,
+  }) {
+    return {
+      posts: [
+        {title: 'title', content: 'content', createAt: new Date()}
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.posts-page{
+.posts-page {
   display: flex;
   justify-content: center;
   align-items: center;
-  &__header{
+  &__header {
     min-height: 20px;
     background-color: antiquewhite;
   }
