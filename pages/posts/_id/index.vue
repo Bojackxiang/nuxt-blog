79d126b2d6df6405081a91067bbdae2838351267
Alex <alex@q-83.com>
2021-06-07 00:55:31 +1000

    FIX the duplicated commit in the created function

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -3,13 +3,13 @@
     single post id {{ postId }}
     <div class="post-page__content">
       <div class="post-page__title">
-        {{ title }}
+        {{ retrivedPost ? retrivedPost.title : "default title" }}
       </div>
       <div class="post-page__content">
-        {{ content }}
+        {{ retrivedPost ? retrivedPost.content : "default content" }}
       </div>
       <div class="post-page__createAt">
-        {{ createdAt }}
+        {{ retrivedPost ? retrivedPost.createdAt : "default createdAt" }}
       </div>
       <div class="post-page__error" v-show="Boolean(error)">
         {{ error }}
@@ -20,6 +20,7 @@
 
 <script>
 import axios from "axios";
+
 export default {
   data() {
     // NOTE 使用 data 来做初始数据的展示
@@ -31,36 +32,37 @@ export default {
       error: "",
     };
   },
-  async asyncData(context) {
+  async fetch() {
     // NOTE 如果在这边直接 throw，会直接进入到 error page
+    // NOTE fetch 里面是可以使用 this 的
+    // 这是和 asyncdata 最不同的地方
     try {
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/todos/1"
       );
       const { data } = response;
 
-      return {
-        content: "post content",
-        title: data.title,
-        createdAt: new Date().toString(),
+      this.content = "post content";
+      this.title = data.title;
+      this.createdAt = new Date().toString();
+
+      // 因为能够 access this，所以可以直接 dispatch
+      const post = {
+        postId: this.postId,
+        content: this.content,
+        title: this.title,
+        createdAt: this.createdAt,
       };
+
+      this.$store.commit("setPosts", [post]);
     } catch (error) {
       return { error: error.message };
     }
   },
-  created() {
-    const post = {
-      postId: this.postId,
-      content: this.content,
-      title: this.title,
-      createdAt: this.createdAt,
-    };
-
-    this.$store.commit("setPosts", [
-      {
-        ...post,
-      },
-    ]);
+  computed: {
+    retrivedPost() {
+      return this.$store.getters.post;
+    },
   },
 };
 </script>
