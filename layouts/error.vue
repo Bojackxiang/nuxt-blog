53637644af6fb71202df6b5aa9f37f25a3f0cb0b
Alex <alex@q-83.com>
2021-06-06 22:27:06 +1000

    添加 error 页面

---
+++ layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>something wrong ...</p>
+
+    <div>
+      <a href="/">go to main page</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
