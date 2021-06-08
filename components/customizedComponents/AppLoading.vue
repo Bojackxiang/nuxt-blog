e94634365aa6a7aadf8bf53516272d908a1dde51
Alex <alex@q-83.com>
2021-06-08 23:51:28 +1000

    使用 mixin 来进行页面的渲染

---
+++ components/customizedComponents/AppLoading.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="loading-wrapper">
+    loading...
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/mixins.scss';
+@import '../../assets/scss/config.scss';
+.loading-wrapper {
+  @include flex();
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0,0.5);
+  z-index: 999;
+  color: $color_white;
+}
+</style>
