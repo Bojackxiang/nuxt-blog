85e955b409a7e9bb8d754ccae25340783b69c6ab
Alex <alex@q-83.com>
2021-06-08 23:53:15 +1000

    添加了一个延时请求

--- utils/formUtils.js
+++ utils/formUtils.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const formClean = (
   inputZValue
 ) => {
@@ -5,3 +7,18 @@ export const formClean = (
   console.log(inputZValue);
 }
 // REVIEW 也就是说可以随意的调用 普通 js 的方法
+
+
+export const delayRequest = (link, time, axiosClient=axios) => {
+  return new Promise((resolve, reject)=>{
+    setTimeout(async () => {
+      try {
+        const response = await axiosClient.get(link)  
+        resolve(response)
+      } catch (error) {
+        reject(error)
+      }
+      
+    }, time);
+  })
+}
