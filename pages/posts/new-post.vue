435500900617e87213b5ac834cea33f307b9eb0a
Alex <alex@q-83.com>
2021-06-09 15:21:08 +1000

    修复了 部分 bug

--- pages/posts/new-post.vue
+++ pages/posts/new-post.vue
@@ -1,26 +1,20 @@
 <template>
   <div>
     create a new post
-    <button @click="createNewPost">create a new</button>
+    <AppButton @click="createNewPost">create a new</AppButton>
   </div>
 </template>
 
 <script>
-import { delayRequest } from '../../utils/formUtils';
+import DelayRequest from "../../utils/formUtils";
+
 export default {
   methods: {
     async createNewPost() {
       // REVIEW 由此可见，commit 是一个 sync 的方法
-      this.$store.dispatch('TOGGLE_LOADING')
+      this.$store.dispatch("TOGGLE_LOADING");
 
-      await delayRequest(
-        'https://jsonplaceholder.typicode.com/posts', 
-        5000, 
-        this.$axios
-      )
-      
-      this.$store.dispatch('TOGGLE_LOADING')
-      
+      this.$store.dispatch("TOGGLE_LOADING");
     },
   },
 };
